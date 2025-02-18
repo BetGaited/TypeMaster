a0ba4e07b08bb8d18a0056548cad670029aa6b7c
Ana Perez <deidrysanchez01@gmail.com>
2025-02-18 19:20:33 -0400

    I want to see if it stops being gray

--- typemaster.py
+++ typemaster.py
@@ -1,1 +1,10 @@
 import random
+
+def main(page: ft.Page):
+    words = ["Sonic", "Shadow", "Silver", "Amy", "Knuckles", "Tails", "Blaze", "Rouge", "Omega", "Cream", "Vector", "Espio",
+            "Charmy", "Eggman", "Metal"]
+    random.shuffle(words)
+    words = words[:15]
+
+    current_index = 0
+    mistakes = 0
